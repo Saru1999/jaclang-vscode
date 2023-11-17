ff9cf773a621109f854828bc56f73711bfa0fe8a
marsninja <jason@mars.ninja>
2023-11-17 07:35:27 -0500

    Formatting not fully ready

--- bundled/tool/common/format.py
+++ bundled/tool/common/format.py
@@ -5,12 +5,12 @@ from jaclang.jac.transpiler import jac_str_to_pass
 
 
 def format_jac(source: str) -> str:
-    try:
-        return jac_str_to_pass(
-            jac_str=source,
-            file_path="",
-            target=JacFormatPass,
-            schedule=[JacFormatPass],
-        ).ir.gen.jac
-    except Exception:
-        return source
+    # try:
+    #     return jac_str_to_pass(
+    #         jac_str=source,
+    #         file_path="",
+    #         target=JacFormatPass,
+    #         schedule=[JacFormatPass],
+    #     ).ir.gen.jac
+    # except Exception:
+    return source
