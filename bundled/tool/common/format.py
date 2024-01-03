b8d4e1ffe32a04726c57b9f76f0e5c97caa777e0
marsninja <jason@mars.ninja>
2024-01-03 15:56:36 -0500

    Auto formatter now in, here we go

--- bundled/tool/common/format.py
+++ bundled/tool/common/format.py
@@ -1,16 +1,17 @@
 from jaclang.compiler.passes.tool import (
+    FuseCommentsPass,
     JacFormatPass,
 )
 from jaclang.compiler.transpiler import jac_str_to_pass
 
 
 def format_jac(source: str) -> str:
-    # try:
-    #     return jac_str_to_pass(
-    #         jac_str=source,
-    #         file_path="",
-    #         target=JacFormatPass,
-    #         schedule=[JacFormatPass],
-    #     ).ir.gen.jac
-    # except Exception:
-    return source
+    try:
+        return jac_str_to_pass(
+            jac_str=source,
+            file_path="",
+            target=JacFormatPass,
+            schedule=[FuseCommentsPass, JacFormatPass],
+        ).ir.gen.jac
+    except Exception:
+        return source
